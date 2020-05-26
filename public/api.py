d54c749fde194e51c5158314542df6a94284bf2f
Aswin Visva <aswinvisva@gmail.com>
2020-05-26 00:42:02 -0400

    First draft for the dashboard creation feature

--- public/api.py
+++ public/api.py
@@ -1,8 +1,13 @@
 import io
 import os
+from math import pi
 
 import flask
-from flask import jsonify, request, Response, render_template
+from bokeh.models import ColumnDataSource
+from bokeh.palettes import Category20c
+from bokeh.plotting import figure
+from bokeh.transform import cumsum
+from flask import jsonify, request, Response, render_template, redirect
 from tensorflow.python.keras.models import load_model
 import numpy as np
 import cv2
@@ -11,14 +16,76 @@ from tensorflow.keras.applications import imagenet_utils, inception_v3
 from PIL import Image
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
+import uuid
+
+import pandas as pd
+
+from os.path import dirname, join
+
+from bokeh.io import curdoc
+from bokeh.embed import components
+from bokeh.models.widgets import Tabs
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+feature_names = ["Negative Result",
+                 "Mild DR",
+                 "Moderate DR",
+                 "Severe DR",
+                 "Proliferate DR"
+                 ]
+
+last_prediction = []
+last_image = []
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
+
+
+@app.route('/dashboard/')
+def show_dashboard():
+    print(last_prediction)
+
+    x = {
+        'No DR':last_prediction[0][0],
+        'Mild DR':last_prediction[0][1],
+        'Moderate DR': last_prediction[0][2],
+        'Severe DR': last_prediction[0][3],
+        'Proliferate DR': last_prediction[0][4],
+    }
+
+    data = pd.Series(x).reset_index(name='value').rename(columns={'index': 'country'})
+    data['angle'] = data['value'] / data['value'].sum() * 2 * pi
+    data['color'] = Category20c[len(x)]
+
+    p = figure(plot_height=350, title="AI Prediction", toolbar_location=None,
+               tools="hover", tooltips="@country: @value", x_range=(-0.5, 1.0))
+
+    p.wedge(x=0, y=1, radius=0.4,
+            start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+            line_color="white", fill_color='color', legend_field='country', source=data)
+
+    imgArray = last_image[0].convert('RGBA')
+    xdim, ydim = imgArray.size
+
+    img = np.empty((ydim, xdim), dtype=np.uint32)
+    view = img.view(dtype=np.uint8).reshape((ydim, xdim, 4))
+    # Copy the RGBA image into view, flipping it so it comes right-side up
+    # with a lower-left origin
+    view[:, :, :] = np.flipud(np.asarray(imgArray))
+
+    dim = max(xdim, ydim)
+    image = figure(title="Uploaded Image",x_range=(0,dim), y_range=(0,dim))
+    image.image_rgba(image=[img],x=0, y=0, dw=xdim, dh=ydim)
+
+    script, div = components(p)
+    image_script, image_div = components(image)
+
+    return render_template('dashboard.html', script=script,
+                           div=div, script2=image_script, div2=image_div, patient_id=uuid.uuid1())
+
 
 @app.route('/api/v1/predictor/diagnosis', methods=['POST'])
 def api_diagnosis():
@@ -47,7 +114,11 @@ def api_diagnosis():
             # classify the input image and then initialize the list
             # of predictions to return to the client
             prediction = model.predict(image)
-            print(prediction)
+
+            if len(last_prediction) > 0:
+                last_prediction.pop()
+
+            last_prediction.append(prediction[0])
 
             # loop over the results and add them to the list of
             # returned predictions
@@ -57,11 +128,13 @@ def api_diagnosis():
 
             # indicate that the request was a success
             data["success"] = True
+
         else:
             print("No image!")
 
     # return the data dictionary as a JSON response
     return flask.jsonify(data)
+
 
 def image_preprocessing(image):
     # if the image mode is not RGB, convert it
@@ -69,13 +142,21 @@ def image_preprocessing(image):
         image = image.convert("RGB")
 
     # resize the input image and preprocess it
-    image = image.resize((224,224))
+    image = image.resize((224, 224))
+
+    if len(last_image) > 0:
+        last_image.pop()
+
+    last_image.append(image)
+
     image = img_to_array(image)
+
     image = inception_v3.preprocess_input(image)
 
     image = np.expand_dims(image, axis=0)
 
     return image
+
 
 def prepare_image(image, target=(224, 224)):
     # if the image mode is not RGB, convert it
@@ -92,5 +173,6 @@ def prepare_image(image, target=(224, 224)):
 
     # return the processed image
     return image
+
 
 app.run()
